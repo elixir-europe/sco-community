48eeb723625d19c179bf18ef489389c5356d6772
Czarnewski <paulo.czarnewski@scilifelab.se>
2023-02-21 14:56:55 +0100

    adds ELIXIR template

---
+++ docs/jekyll.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# using the official Jekyll image, see https://github.com/envygeeks/jekyll-docker
+# runs on port 4000
+
+
+mkdir -p ".bundles_cache"
+docker run --rm \
+  -v "$PWD:/srv/jekyll" \
+  -e BUNDLE_PATH="/srv/jekyll/.bundles_cache" \
+  -p 4000:4000 \
+  jekyll/builder:3.8 \
+  bash -c "gem install bundler && bundle install && bundle exec Jekyll serve --host 0.0.0.0 --verbose --config _config.yml,_config_dev.yml"
---
+++ docs/pages/training/index.md
@@ -0,0 +1,6 @@
+---
+layout: default
+title: Training
+nav_order: 1
+has_children: true
+---
